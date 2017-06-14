0c6e2fdbd4a82e674fc823df923048dfcaca80db
eyaleatafekaacil <eyale@afeka.ac.il>
2017-06-14 05:05:50 +0300

    updated printed message

--- src/hello/Application.java
+++ src/hello/Application.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Application {
 	public static void main(String[] args) {
-		System.err.println("Hello World!");
+		System.err.println("Hello Standalone Java App!");
 	}
 }
